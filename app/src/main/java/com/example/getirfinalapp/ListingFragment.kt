955ed483099e03c708cacd71e962f76995a3e627
Muhammet Emin Gundogar <407596@ogr.ktu.edu.tr>
2024-04-23 13:55:30 +0300

    Toolbar Updated

--- app/src/main/java/com/example/getirfinalapp/ListingFragment.kt
+++ app/src/main/java/com/example/getirfinalapp/ListingFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -14,6 +18,7 @@ import com.example.getirfinalapp.databinding.FragmentListingBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+
 @AndroidEntryPoint
 class ListingFragment : Fragment(R.layout.fragment_listing) {
 
@@ -34,18 +39,26 @@ class ListingFragment : Fragment(R.layout.fragment_listing) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val toolbar = activity?.findViewById<Toolbar>(R.id.myToolbar)
+        toolbar?.findViewById<ImageView>(R.id.iv_cancel)?.visibility = View.INVISIBLE
+
+
+
+
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.data.collect { resource ->
-                    when(resource){
+                    when (resource) {
                         is Resource.Success -> {
-                           val data = resource.data
+                            val data = resource.data
                             // Recycler View veri gösterilecek.
 
                         }
+
                         is Resource.Error -> {
                             val errorMessage = resource.message
                         }
+
                         is Resource.Loading -> {
                             // Loading işlemi
                         }
